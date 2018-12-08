3023f2b89cbaebbbdd914dd712761395436be547
John Mark Redding <johnmarkredding@gmail.com>
2018-12-08 23:50:57 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 class Application
+=======
+class App
+>>>>>>> d1b9d9b91dcdedf325b47de3dae1cca52aaf7c10
   def call(env)
     resp = Rack::Response.new
     resp.write choose_greeting
@@ -7,10 +11,17 @@ class Application
   
   def choose_greeting
     greeting = ""
+<<<<<<< HEAD
     if (Time.now.hour <= 12)
       greeting = "Good Morning!"
     else
       greeting = "Good Afternoon!"
+=======
+    if (Time.new.hour) > 12
+      greeting = "Good Afternoon!"
+    else
+      greeting = "Good Morning!"
+>>>>>>> d1b9d9b91dcdedf325b47de3dae1cca52aaf7c10
     end
     greeting
   end
